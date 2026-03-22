22fc309e91fa9d266e5d1e94aeb4e4dee7036ae2
Renato Motikawa <renatohidekim@gmail.com>
2026-03-22 12:30:41 -0300

    Remove all comments from project

--- playwright-fixture.ts
+++ playwright-fixture.ts
@@ -1,3 +1,1 @@
-// Re-export the base fixture from Playwright
-// Override or extend test/expect here if needed
 export { test, expect } from "@playwright/test";
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,6 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  // Add your custom playwright configuration overrides here
-  // Example:
-  // timeout: 60000,
-  // use: {
-  //   baseURL: 'http://localhost:3000',
-  // },
 };
 
 export default config;
